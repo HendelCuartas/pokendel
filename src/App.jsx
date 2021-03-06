864c5bab36cbff322896f0edfe4a5a9117c4da9b
Hendel Cuartas <hendel_10@hotmail.com>
2021-03-06 13:10:50 -0700

    Setup 🙌

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import PokemonContainer from 'components/PokemonContainer';
+import './App.styles.css';
+// import PokemonContainer from "components/PokemonContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PokemonContainer/PokemonContainer.component.tsx
@@ -0,0 +1,9 @@
+import "./pokemonContainer.styles.scss";
+
+const PokemonContainer: React.FC = () => {
+    return (
+        <div className="container"></div>
+    )
+};
+
+export default PokemonContainer;
